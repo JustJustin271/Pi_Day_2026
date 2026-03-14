06727341b5cc15cb8b190270ea6481e67576dc5b
Orangesicle <orangesicle213@gmail.com>
2026-03-14 15:00:46 -0400

    Create pi_nth_place.rkt

---
+++ pi_nth_place.rkt
@@ -0,0 +1,53 @@
+;; Happy Pi Day 2026!
+;; March 14, 2026 :)
+
+(define pi-1024
+  (string-append
+  "3"
+  "14159265358979323846264338327950288419716939937510" ;;This is pi (Up to 1024 digits) :D
+  "58209749445923078164062862089986280348253421170679" ;; Each row is 50 digits :)
+  "82148086513282306647093844609550582231725359408128" ;; I checked :)
+  "48111745028410270193852110555964462294895493038196"
+  "44288109756659334461284756482337867831652712019091"
+  "45648566923460348610454326648213393607260249141273"
+  "72458700660631558817488152092096282925409171536436"
+  "78925903600113305305488204665213841469519415116094"
+  "33057270365759591953092186117381932611793105118548"
+  "07446237996274956735188575272489122793818301194912"
+  "98336733624406566430860213949463952247371907021798"
+  "60943702770539217176293176752384674818467669405132"
+  "00056812714526356082778577134275778960917363717872"
+  "14684409012249534301465495853710507922796892589235"
+  "42019956112129021960864034418159813629774771309960"
+  "51870721134999999837297804995105973173281609631859" ;;Feynman point? 999999 :)
+  "50244594553469083026425223082533446850352619311881"
+  "71010003137838752886587533208381420617177669147303"
+  "59825349042875546873115956286388235378759375195778"
+  "18577805321712268066130019278766111959092164201989"
+  "38095257201065485863278"))
+
+;; Source: https://www.cecm.sfu.ca/organics/papers/borwein/paper/html/local/bdigits.html
+
+
+;; pi-digit : num or string -> string
+;; Takes in a digits place of pi and outputs the nth digit in pi
+;; Or takes in joke answers of strings like "pie" and outputs funny response
+;; Starts at one, not 0 :)
+;; So the 1st digit of pi is 3, not 1 :))
+(define (pi-digit nth)
+  (cond
+    [(and (integer? nth) (<= nth 1024) (>= nth 1)) (string-ith pi-1024 (- nth 1))]
+    [(integer? nth) "Digit place not available in system"]
+    [(and (number? nth)(= nth pi)) "Good luck trying to find the pi-th digit in pi :)"]
+    [(string=? "pie" nth) "Wrong kind of pie :)"]
+    [else "Invalid input"]))
+
+(check-expect (pi-digit 1) "3")
+(check-expect (pi-digit 1024) "8")
+(check-expect (pi-digit 1025) "Digit place not available in system")
+(check-expect (pi-digit pi) "Good luck trying to find the pi-th digit in pi :)")
+(check-expect (pi-digit "pie") "Wrong kind of pie :)")
+(check-expect (pi-digit "apple") "Invalid input")
+
+;; March 14, 2026
+;; Happy Pi Day :D
